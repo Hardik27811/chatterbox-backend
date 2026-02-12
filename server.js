f2c33fe43f63ca84bd30e4518248036b8f3cecd2
Hardik Sharma <kumarankit2002dseu@gmail.com>
2026-02-12 17:28:37 +0530

    updated url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.set("io",io) // Still makes io available in routes
 
 //middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials : true
 }))
 app.use(cookieParser())
